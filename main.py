7840781c56159467018c2d6e432bdc82c47ac01d
Millad Ayoubi <Milladayoubi@gmail.com>
2021-03-02 02:22:17 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,91 @@
+
+
+# weight = int(input('Enter Your Weight: (Pound To Kilo Converter)'))
+
+
+# def weightCalculate(weight):
+# if(weight > 0):
+# return round(weight * 0.4535, 1)
+
+
+# print(str(weight) + " Pounds == " + str(weightCalculate(weight)) + " KG")
+
+# secret_number = 9
+
+# answer = 0
+
+# while answer != secret_number:
+# print("Guess a number between 1 and 10")
+# answer = int(input())
+
+# print("Correct!")
+
+
+#command = " "
+#print("Enter A Car Command")
+
+# while command != "quit":
+#command = input(">").lower()
+
+# if(command == "start"):
+#   print("Car Started...")
+# elif (command == "stop"):
+#     print("Car stops")
+# elif (command == "quit"):
+# break
+#  else:
+#   print("Command Not Understood")
+
+#print("Operation end")
+
+
+#prices = [24, 32, 12]
+#total = 0
+#count = 1
+# for i in prices:
+
+#print(f"Price  {count} is {i}")
+#total += i
+#count += 1
+
+
+#print(f"Total Price is {total}")
+
+
+#numbers = [5, 2, 5, 2, 2]
+
+# for i in numbers:
+#print('x' * i)
+
+#numbers = [3, 6, 2, 8, 4, 10, 2, 3, 5, 677, 23, 23, 4324]
+#tmp = numbers[0]
+
+
+# for i in numbers:
+# if(i > tmp):
+#tmp = i
+
+
+#print(f'The Largest number in the list is {tmp}')
+
+c = {
+    "name": "Millad",
+    "age": 30,
+    "isverified": True
+}
+
+phone = input('Phone:')
+
+
+map1 = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three"
+}
+
+output = ""
+
+for i in phone:
+    print(map1[i])
+
+print(output)
